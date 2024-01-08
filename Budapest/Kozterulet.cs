4331296c3466a56c2595371d0d37249797a94936
tothtamas9 <71500475+tothtamas9@users.noreply.github.com>
2024-01-08 18:31:24 +0100

    Add project files.

---
+++ Budapest/Kozterulet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budapest
+{
+    public class Kozterulet
+    {
+        int iranyitoszam;
+        string nev;
+        string utotag;
+        string ker;
+
+        public Kozterulet(int iranyitoszam, string nev, string utotag, string ker)
+        {
+            this.iranyitoszam = iranyitoszam;
+            this.nev = nev;
+            this.utotag = utotag;
+            this.ker = ker;
+        }
+
+        public int Iranyitoszam { get => iranyitoszam; }
+        public string Nev { get => nev; }
+        public string Utotag { get => utotag; }
+        public string Ker { get => ker; }
+    }
+}
---
+++ Budapest/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Budapest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. feladat:
+            List<Kozterulet> kozteruletek = new List<Kozterulet>();
+            string[] olvasottSorok = File.ReadAllLines("Source\\bp.csv");
+
+            for (int i = 1; i < olvasottSorok.Length; i++)
+            {
+                string[] tagok = olvasottSorok[i].Split(';');
+                Kozterulet ujKozterulet = new Kozterulet(int.Parse(tagok[0]), tagok[1], tagok[2], tagok[3]);
+                kozteruletek.Add(ujKozterulet);
+            }
+
+            //2. feladat:
+            Console.WriteLine("2. feladat");
+            Console.WriteLine($"\tTételek száma: {kozteruletek.Count()}");
+
+
+            //3. feladat:
+            Console.WriteLine("3. feladat");
+            int kTeruletekSzama = 0;
+            foreach (var i in kozteruletek)
+            {
+                if (i.Ker == "XIII")
+                {
+                    kTeruletekSzama += 1;
+                }
+            }
+            Console.WriteLine($"\tKözterületek száma a XIII. kerületben:{kTeruletekSzama}");
+
+            //4. feladat:
+            Console.WriteLine("4. feladat");
+            Console.Write("Kérek egy irányítószámot! ");
+            int kertIRSZ = Convert.ToInt32(Console.ReadLine());
+
+
+            //5. feladat:
+            Console.WriteLine("5. feladat");
+            Console.Write("Kérem a közterület nevének kezdetét! ");
+            string ktNeve = Convert.ToString(Console.ReadLine());
+
+            foreach (var i in kozteruletek)
+            {
+                if (i.Nev.Contains(ktNeve))
+                {
+                    Console.WriteLine($"{i.Nev} - {i.Ker}. kerület");
+                }
+            }
+
+        }
+    }
+}
